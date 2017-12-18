39f95b629a7877ad0544b805947c903667e646ea
zhijun <northeast80@gmail.com>
2017-12-18 13:58:49 +0800

    test java hello world

---
+++ ssd/src/ssd/test1.java
@@ -0,0 +1,12 @@
+package ssd;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// test
+		System.out.println("hello");
+		System.out.println("again");
+	}
+
+}
